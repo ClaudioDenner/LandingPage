8b529cc76f3f4ddf5ec6efb714f87fbacc9dd60e
ClaudioDenner <78771522+ClaudioDenner@users.noreply.github.com>
2023-02-05 22:40:48 -0300

    "semi-finalizado"

--- js/popUpImg.js
+++ js/popUpImg.js
@@ -1,19 +1,57 @@
-document.addEventListener('keydown', function(tecla){ if(tecla.keyCode==27){downPopUpImg()}});
-//imgs.addEventListener("click",function(){console.log('clicou')})
-var imgs = document.querySelectorAll('#ItemUrlImg')
-imgs.forEach(imagens=>{
+    document.addEventListener('keydown', function(tecla){ if(tecla.keyCode==27){downPopUpImg()}});
+    //imgs.addEventListener("click",function(){console.log('clicou')})
+    var imgs = document.querySelectorAll('#ItemUrlImg')
+    imgs.forEach(imagens=>{
 
     imagens.addEventListener("click",function(){
-        let newUrl = imagens.src.toString()
-        alterUrlPopUp(newUrl)
+        newUrl = imagens.src.toString()
+        
+        idProduct = imagens.getAttribute('dataId').toString()
+        nameProduct = imagens.getAttribute('dataName').toString()
+        descricaoProduct = imagens.getAttribute('dataDescricao').toString()
+        tamanhosProduct = imagens.getAttribute('dataTamanhos').toString()
+        quantParcelasProduct = imagens.getAttribute('dataQuantParcelasSemJuros').toString()
+        valorProduct = imagens.getAttribute('dataValor').toString()
+
+
+
+
+        alterUrlPopUp(newUrl, idProduct,nameProduct,descricaoProduct,tamanhosProduct,quantParcelasProduct, valorProduct)
+
+
+
+
     })
 
 
-function alterUrlPopUp(newUrl){
-url = newUrl
-let imgPopUp = document.getElementById('imgPopUp')
 
-imgPopUp.setAttribute('src',url)
+function alterUrlPopUp(newUrl,idProduct,nameProduct,descricaoProduct,tamanhosProduct,quantParcelasProduct, valorProduct){
+    
+    url = newUrl
+    let imgPopUp = document.getElementById('imgPopUp')
+    
+    imgPopUp.setAttribute('src',url)
+
+
+    //setando detalhes do produto
+    id = idProduct
+    name = nameProduct
+    descricao = descricaoProduct
+    tamanhos = tamanhosProduct
+    valor = valorProduct
+    parcelas = quantParcelasProduct
+
+
+    document.getElementById('idProductDetails').innerText = id
+    document.getElementById('nameProductDetails').innerText = name
+    document.getElementById('descricaoProductDetails').innerText = descricao
+    document.getElementById('tamanhosProductDetails').innerText = tamanhos
+    document.getElementById('valorProductDetails').innerText = valor
+    document.getElementById('parcelasProductDetails').innerText = parcelas
+
+
+
+
 
 
 }   
